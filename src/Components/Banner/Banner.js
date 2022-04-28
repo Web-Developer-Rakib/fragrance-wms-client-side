732ca68f32db151b301291c9814039d70d34faea
WebDeveloperApolo <rakibodesk7@gmail.com>
2022-04-29 00:48:14 +0600

    Banner section created

---
+++ src/Components/Banner/Banner.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { Carousel } from "react-bootstrap";
+import CaroImg1 from "../../Images/caru-1.jpg";
+import CaroImg2 from "../../Images/caru-2.jpg";
+import CaroImg3 from "../../Images/caru-3.jpg";
+import "./Banner.css";
+
+const Banner = () => {
+  return (
+    <Carousel>
+      <Carousel.Item>
+        <img className="d-block w-100" src={CaroImg1} alt="First slide" />
+        <Carousel.Caption>
+          <h3>Alfred Ritchy</h3>
+          <p>
+            The Alfred Ritchy fragrance collection, recently released in France,
+            builds on the slogan "How wonderful it is when adults believe in
+            magic."{" "}
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img className="d-block w-100" src={CaroImg2} alt="Second slide" />
+
+        <Carousel.Caption>
+          <h3>Scratchy, Vegetal Freshness</h3>
+          <p>
+            The overall aesthetic of Haute Fraîcheur is much sharper and less
+            rich than its elder pillar.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img className="d-block w-100" src={CaroImg3} alt="Third slide" />
+
+        <Carousel.Caption>
+          <h3>Dior Miss Dior Rose Essence</h3>
+          <p>
+            The fragrance is a first-of-its-kind yearly limited edition infused
+            with floral notes from the year’s seasonal harvest.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+};
+
+export default Banner;
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,17 +1,18 @@
 import React from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
+import "./Header.css";
 
 const Header = () => {
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+    <Navbar collapseOnSelect expand="lg" variant="none">
       <Container>
         <Navbar.Brand href="#home">Fragrance WMS</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#features">Manage Items</Nav.Link>
-            <Nav.Link href="#pricing">Add Item</Nav.Link>
-            <Nav.Link href="#pricing">My Items</Nav.Link>
+            <Nav.Link href="#1">Add Item</Nav.Link>
+            <Nav.Link href="#">My Items</Nav.Link>
           </Nav>
           <Nav>
             <Nav.Link href="#deets">Login</Nav.Link>
