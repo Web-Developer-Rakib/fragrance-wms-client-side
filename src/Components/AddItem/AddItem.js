6cc3de959c24f386afab00788cce661586c7ed99
WebDeveloperApolo <rakibodesk7@gmail.com>
2022-05-01 20:26:42 +0600

    Add item functionality created

--- src/Components/AddItem/AddItem.js
+++ src/Components/AddItem/AddItem.js
@@ -1,24 +1,84 @@
 import React from "react";
+import { toast } from "react-toastify";
 import "./AddItem.css";
 
 const AddItem = () => {
+  const addItem = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const name = e.target.name.value;
+    const productName = e.target.productName.value;
+    const price = e.target.price.value;
+    const description = e.target.description.value;
+    const quantity = e.target.quantity.value;
+    const photoURL = e.target.photoURL.value;
+    const product = {
+      email,
+      name,
+      productName,
+      price,
+      description,
+      quantity,
+      photoURL,
+    };
+    //Post data
+    if (
+      email === "" ||
+      name === "" ||
+      productName === "" ||
+      price === "" ||
+      description === "" ||
+      quantity === "" ||
+      photoURL === ""
+    ) {
+      toast.warn("Please fill up all fields.");
+    } else {
+      fetch("http://localhost:5000/add-product", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(product),
+      })
+        .then((response) => response.json())
+        .then(() => {
+          toast.success("added successfully.");
+          e.target.reset();
+        })
+        .catch(() => {
+          toast.error("Something went wrong.");
+        });
+    }
+  };
   return (
     <div className="add-item-page">
-      <form className="add-item-form">
+      <form onSubmit={addItem} className="add-item-form">
         <h3>ADD ITEM</h3>
-        <input type="email" className="d-none" />
+        <input
+          type="text"
+          value={"ggmail@gmail.com"}
+          name="email"
+          className="d-none"
+        />
+        <input type="text" name="name" value={"name"} className="d-none" />
         <br />
-        <input type="text" className="d-none" />
+        <input
+          type="text"
+          name="productName"
+          placeholder="Enter the peoduct name"
+        />
         <br />
-        <input type="text" placeholder="Enter the peoduct name" />
+        <input type="number" name="price" placeholder="Enter the price" />
         <br />
-        <input type="text" placeholder="Enter the price" />
+        <textarea
+          type="text"
+          name="description"
+          placeholder="Enter products description"
+        />
         <br />
-        <textarea type="text" placeholder="Enter products description" />
+        <input type="number" name="quantity" placeholder="Enter the quantity" />
         <br />
-        <input type="text" placeholder="Enter the quantity" />
-        <br />
-        <input type="text" placeholder="Enter the image URL" />
+        <input type="url" name="photoURL" placeholder="Enter the image URL" />
         <br />
         <button type="submit" className="add-item-btn">
           ADD ITEM
