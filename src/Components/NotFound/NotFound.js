1cc6e28af08273f6c682402bc0c23dbd367ec1e9
WebDeveloperApolo <rakibodesk7@gmail.com>
2022-05-06 16:31:50 +0600

    Not found page modified

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -1,9 +1,10 @@
 import React from "react";
+import "./NotFound.css";
 
 const NotFound = () => {
   return (
-    <div>
-      <h1>404 Not found</h1>
+    <div className="not-found">
+      <h1 className="text-danger">404 NOT FOUND!</h1>
     </div>
   );
 };
